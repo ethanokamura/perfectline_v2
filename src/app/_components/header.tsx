135db8e06ef3a2254e175f754e721e25e09c8f6c
Ethan <ethanokamura3@gmail.com>
2025-03-22 22:03:27 -0700

    implemented the main header on the home page

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,18 @@
+import styles from "./header.module.css"
+
+type Props = {
+  title: string;
+  subtitle: string;
+  description: string;
+};
+
+export default async function Header({ title, subtitle, description }: Props) {
+  return (
+    <header className={styles.header}>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      <h3>{description}</h3>
+    </header>
+  );
+}
+
