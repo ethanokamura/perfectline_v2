ab92835561237b49372675e62603f17cda5adcad
Ethan <ethanokamura3@gmail.com>
2025-03-25 13:21:00 -0700

    added new fetch method to get courses

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,24 +1,28 @@
-import { formatCourses } from "@/lib/formatter";
-import { Course } from "@/src/interfaces/course";
-import Card from "../_components/card";
-import style from "./course-list.module.css";
-import Header from "../_components/header";
+import React from 'react';
+import { Course } from '@/src/interfaces/course'; // Assuming Course interface
+import Card from '../_components/card';
+import style from './course-list.module.css';
+import Header from '../_components/header';
 
-async function getCourses(): Promise<Course[]> {
-  const data = await fetch('http://localhost:3000/api/courses').then((res) => res.json());
-  return data.courses; // Extract courses array
+async function getCourses() {
+  const data = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/courses', {
+    cache: "no-cache",
+  }).then((res) => res.json());
+  if (data == null) {
+    throw new Error('Failed to fetch courses');
+  }
+  return data.courses;
 }
 
 export default async function CourseListPage() {
   const courses: Course[] = await getCourses();
-  const formattedCourses = formatCourses(courses);
 
   return (
     <main className={style.main}>
       <Header title="PerfectLine" subtitle="Courses" description="A list of courses we offer:" />
       <div className={style.courseList}>
-        {formattedCourses.map((course: Course) => (
-          <Card course={course} />
+        {courses.map((course: Course) => (
+          <Card course={course} key={course.course} />
         ))}
       </div>
     </main>
