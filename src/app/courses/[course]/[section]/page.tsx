78e52d4f80a620b5faa4ac63a1ce196b27789b29
Ethan <ethanokamura3@gmail.com>
2025-03-25 13:21:19 -0700

    updated markdown rendering

--- src/app/courses/[course]/[section]/page.tsx
+++ src/app/courses/[course]/[section]/page.tsx
@@ -2,12 +2,12 @@ import { notFound } from 'next/navigation';
 import { getPageBySlug, getCourseSections } from '@/src/lib/api';
 import SectionList from "@/src/app/_components/section-list";
 import { SectionHeader } from '@/src/app/_components/section-header';
-import SectionBody from '@/src/app/_components/section-body';
+import { SectionBody } from '@/src/app/_components/section-body';
 
 export default async function Page({ params }: { params: Promise<{ course: string, section: string }> }) {
   const { course, section } = await params;
-  let page = getPageBySlug(course, section);
-  const sections = getCourseSections(course);
+  const page = await getPageBySlug(course, section);
+  const sections = await getCourseSections(course);
   if (!page) {
     return notFound();
   }
