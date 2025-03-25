8edd8bd22532785495b27ea3444921c879b75165
Ethan <ethanokamura3@gmail.com>
2025-03-25 14:06:23 -0700

    added about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,20 @@
-export default function AboutPage({ }) {
+import { notFound } from 'next/navigation';
+import { getInfoPageBySlug } from '@/src/lib/api';
+import { SectionHeader } from '@/src/app/_components/markdown/section-header';
+import { SectionBody } from '@/src/app/_components/markdown/section-body';
 
+export default async function AboutPage() {
+  const page = await getInfoPageBySlug("about");
+  if (!page) {
+    return notFound();
+  }
   return (
-    <main>
-      <h1>Admin Page</h1>
+    <main >
+      {/* Header */}
+      <SectionHeader title={page.title} description={page.description} />
+      <hr/>
+      {/* Body */}
+      <SectionBody content={page.content} />
     </main>
   );
 }
