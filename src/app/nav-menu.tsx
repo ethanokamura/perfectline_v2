808915a96da8895067723f27097b3baed11bbe31
Ethan <ethanokamura3@gmail.com>
2025-03-25 13:16:38 -0700

    update nav bar'

--- src/app/nav-menu.tsx
+++ src/app/nav-menu.tsx
@@ -1,26 +1,24 @@
 import Link from "next/link";
-import Image from "next/image";
 import styles from "./nav-menu.module.css";
+import { SignInButton } from "./_components/buttons";
+import ThemedImage from "./_components/theme-image";
 
 export default function NavMenu() {
   return  (
     <nav className={styles.nav} >
       <Link href={'/'}>
-        <Image
-          src="/dark-logo.svg"
-          width={48}
-          height={48}
-          alt="PerfectLine Logo"
-        />
+      <ThemedImage
+        light="/light-logo.svg"
+        dark="/dark-logo.svg"
+        x={48} 
+        y={48}
+        alt="PerfectLine Logo"
+      />
       </Link>
-      <ul className={styles.links}>
-        <li>
-          <Link href={'/about'}>About</Link>
-        </li>
-        <li>
-          <Link href={'/courses'}>Courses</Link>
-        </li>
-      </ul>
+      <div className={styles.menu}>
+        <Link href={'/courses'}>Courses</Link>
+        <SignInButton />
+      </div>
     </nav>
   );
 }
