37d1bde06df3a426161316bd6688e4504c9aecc1
Ethan <ethanokamura3@gmail.com>
2025-03-25 13:16:06 -0700

    added themed-image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import Image from "next/image";
 import * as config from "@/lib/constants";
 import Header from "./_components/header";
 import style from "./home.module.css";
+import ThemedImage from "./_components/theme-image";
 
 export default function HomePage() {
   return (
     <main className={style.home}>
-        <Image
-          src="/dark-logo.svg"
-          width={128}
-          height={128}
-          alt="PerfectLine Logo"
-        />
+      <ThemedImage
+        light="/light-logo.svg"
+        dark="/dark-logo.svg"
+        x={128} 
+        y={128}
+        alt="PerfectLine Logo"
+      />
       <Header title={config.title} subtitle="Coding Academy" description={config.description} />
       <p>Unlock your potential in the exciting world of software development and embark on a transformative educational journey.</p>
     </main>
